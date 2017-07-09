420c40bc6bb69959ab79f21c5a79c08de0c1b369
lvxiaodong <lvxiaodong@meituan.com>
2017-07-09 20:03:30 +0800

    add hello world

---
+++ src/main/java/com/lxd/test/base/JavaDemo.java
@@ -0,0 +1,10 @@
+package com.lxd.test.base;
+
+/**
+ * Created by lvxiaodong on 2017/7/9.
+ */
+public class JavaDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
